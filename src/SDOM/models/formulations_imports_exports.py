8a74f61d8327236b7c2d0ff1e27601f3f45f572b
Sebastián M <104728742+SebastianManriqueM@users.noreply.github.com>
2025-10-07 21:29:21 -0600

    Add import/export constraints and costs in the Obj. Function

--- src/SDOM/models/formulations_imports_exports.py
+++ src/SDOM/models/formulations_imports_exports.py
@@ -1,6 +1,7 @@
-from pyomo.core import Var, Constraint
+from pyomo.core import Var, Expression, Constraint
 from pyomo.environ import *
 from .models_utils import get_filtered_ts_parameter_dict
+from ..io_manager import get_formulation
 
 ####################################################################################|
 # ----------------------------------- Parameters -----------------------------------|
@@ -15,7 +16,7 @@ def add_import_export_ts_parameters( block,
     filtered_selected_data = get_filtered_ts_parameter_dict(hourly_set, data, key_ts, key_col)
     if key_ts in ["cap_imports", "cap_exports"]:
         block.ts_capacity_parameter = Param( hourly_set, initialize = filtered_selected_data)
-    elif key_ts in ["price_imports", "cost_exports"]:
+    elif key_ts in ["price_imports", "price_exports"]:
         block.ts_price_parameter = Param( hourly_set, initialize = filtered_selected_data)
     return
 
@@ -35,36 +36,62 @@ def add_exports_parameters(model, data: dict):
 ####################################################################################|
 # ------------------------------------ Variables -----------------------------------|
 ####################################################################################|
-def add_generic_import_export_variables(block, *sets, domain=NonNegativeReals, initialize=0):
+def _add_generic_import_export_variables(block, *sets, domain=NonNegativeReals, initialize=0):
     block.variable = Var(*sets, domain=domain, initialize=initialize)
     return
 
 def add_imports_variables(model):
-    add_generic_import_export_variables(model.imports, model.h, domain=NonNegativeReals, initialize=0)
+    _add_generic_import_export_variables(model.imports, model.h, domain=NonNegativeReals, initialize=0)
     return
 
 
 def add_exports_variables(model):
-    add_generic_import_export_variables(model.exports, model.h, domain=NonNegativeReals, initialize=0)
+    _add_generic_import_export_variables(model.exports, model.h, domain=NonNegativeReals, initialize=0)
     return
 
 ####################################################################################|
 # ----------------------------------- Expressions ----------------------------------|
 ####################################################################################|
+def _add_imports_exports_cost_expressions(block, hourly_set, data: dict, component: str):
+    if get_formulation(data, component=component) != "NotModel":
+        block.total_cost = Expression( rule = sum(block.ts_price_parameter[h] * block.variable[h] for h in hourly_set) )
+    else:
+        block.total_cost = Expression( rule = 0 )
+    return
 
+def add_imports_exports_cost_expressions(model, data: dict):
+   
+    _add_imports_exports_cost_expressions(model.imports, model.h, data, 'Imports')
+    _add_imports_exports_cost_expressions(model.exports, model.h, data, 'Exports')
+    return
 
 
 ####################################################################################|
 # ----------------------------------- Constraints ----------------------------------|
 ####################################################################################|
+def _add_imports_exports_capacity_constraint(block, hourly_set):
+    block.capacity_constraint = Constraint(hourly_set, rule=lambda m,h: m.variable[h] <= m.ts_capacity_parameter[h] )
+    return
+
 def add_imports_constraints( model, data: dict ):
+    _add_imports_exports_capacity_constraint(model.imports, model.h)
+    #TODO How to generalize constraint of net load?
+    # model.imports.net_load_constraint = Constraint(
+    #     model.h,
+    #     rule=lambda m, h: m.variable[h] <= max(0, value(m.net_load[h]))
+    # )
+
     return
 
 
 def add_exports_constraints( model, data: dict ):
+    _add_imports_exports_capacity_constraint(model.exports, model.h)
+    #TODO How to generalize constraint of net load? - > for thermal generation
     return
 
 
 ####################################################################################|
 # -----------------------------------= Add_costs -----------------------------------|
 ####################################################################################|
+def add_imports_exports_cost(model):
+    return model.imports.total_cost - model.exports.total_cost

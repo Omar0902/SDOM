86542fbc1fb24ccdb456b2bb8a91b21694d0af88
pesap <pesap@users.noreply.github.com>
2026-01-29 15:14:11 -0700

    fix(ci): fixing build of documentation (#29)
    * build: adding uv style pyproject
    * chore: renaming folder to lowercase
    * fix: updating docs to use uv build

---
+++ src/sdom/__init__.py
@@ -0,0 +1,17 @@
+"""Entry point for sdom."""
+
+from .common.utilities import safe_pyomo_value
+from .config_sdom import configure_logging
+from .io_manager import export_results, load_data
+from .optimization_main import get_default_solver_config_dict, initialize_model, run_solver
+
+__all__ = [
+    "configure_logging",
+    "export_results",
+    "get_default_solver_config_dict",
+    "initialize_model",
+    "load_data",
+    "run_solver",
+    "safe_pyomo_value",
+]
+
